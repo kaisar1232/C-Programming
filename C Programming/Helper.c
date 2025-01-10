d237559537af933a79109e684e16604a9b149ea9
Kaisar Attar <117891197+kaisar1232@users.noreply.github.com>
2025-01-10 16:11:09 +0530

    Delete C Programming directory

--- C Programming/Helper.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int no = 11;
-
-void Marvellous()
-{
-    printf("inside Marvellous function\n");
-    
-}
--- C Programming/PPA.h
+++
@@ -1,7 +0,0 @@
-struct Demo
-{
-    int no;
-    int x;
-};
-
-#define PI 3.14
